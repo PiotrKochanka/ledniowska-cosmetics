1826d3a106d1e6ce642370c7cb23b0fe5e354e84
unknown <kochankap@gmail.com>
2021-12-06 18:54:35 +0100

    Create start.blade.php

---
+++ ledniowska-cosmetics/resources/views/start.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="{{ asset('css/szkielet.css') }}">
+        <title>Strona główna</title>
+    </head>
+    <body>
+        <div class="strona">
+            <header>
+                <div class="animacja-kontener width100">
+                    <div class="animacja-dane width1 block">
+                        <a href="/" class="logo"><img src="../../graphic/logo.png" alt="logo"></a>
+                    </div>
+                </div>
+            </header>
+        </div>
+    </body>
+</html>
